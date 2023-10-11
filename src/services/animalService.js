c757b0c88b682a19abf825d8131b5a2095fcb6b0
Божидар Вълков <125079323+Nock27@users.noreply.github.com>
2023-10-11 19:40:40 +0300

    Added create functionality

--- src/services/animalService.js
+++ src/services/animalService.js
@@ -4,7 +4,11 @@ const baseUrl = 'http://localhost:3030';
 
 
 export const getAll = () => {
-    return requster.get(`${baseUrl}/data/pets?sortBy=_createdOn%20desc&distinct=name`)
+    return requster.get(`${baseUrl}/data/pets?sortBy=_createdOn%20desc&distinct=name`);
+}
+
+export const createOne = (name,breed,age,weight,image) => {
+    return requster.post(`${baseUrl}/data/pets`,{name,breed,age,weight,image});
 }
        
             
