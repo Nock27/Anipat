da22889919a1f4b2414970166aab53f33a0ee703
Bojidar Valkov <nock5631@gmail.com>
2024-03-23 00:53:52 +0200

    Made some cosmetic changes.

--- src/pageViews/create.js
+++ src/pageViews/create.js
@@ -4,7 +4,7 @@ import * as animalService from "../services/animalService.js"
 const createTemplate = (createPetHandler) => html`
     <section id="createPage">
         <form class="createForm" @submit=${createPetHandler}>
-            <img src="./images/cat-create.jpg">
+            <img src="./images/animal-icon-create.png">
             <div>
                 <h2>Create PetPal</h2>
                 <div class="name">
@@ -47,9 +47,9 @@ export const createView = (ctx) => {
         const image = formData.get('image');
         if(name.length !== 0 && breed.length !== 0 && age.length !== 0 && weight.length !== 0 && image.length !== 0){
             animalService.createOne(name,breed,age,weight,image)
-                .then(animal => {
-                    console.log(animal);
-                    ctx.page.redirect('/');
+                .then(() => {
+                    
+                    ctx.page.redirect('/dashboard');
                 })
         }else{
             alert("All fields must be filled");
--- src/pageViews/home.js
+++ src/pageViews/home.js
@@ -6,9 +6,9 @@ import { html } from '../../node_modules/lit-html/lit-html.js'
 const homeTemplate = () => html`
     <section class="welcome-content">
         <article class="welcome-content-text">
-            <h1>We Care</h1>
-            <h1 class="bold-welcome">Your Pets</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+            <h1>Join Us</h1>
+            <h1 class="bold-welcome">Help Pets in Need!</h1>
+            <p>PetSupport Network: Connecting Hearts, Saving Lives!</p>
         </article>
         <article class="welcome-content-image">
             <img src="./images/header-dog.png" alt="dog">
